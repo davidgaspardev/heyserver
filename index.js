2ab8363f67c9865f38281b7bfddc907b67e119f5
David Gaspar <davidgaspar.dev@gmail.com>
2018-05-20 03:05:32 +0000

    Rename Server.js to index.js & update file package.json

--- index.js
+++ index.js
@@ -5,4 +5,8 @@ app.get('/', (rrequest, response) => {
   response.send('Hello World!')
 })
 
+app.get('/recipes', (request, response) => {
+  response.send(request.query.category)
+})
+
 app.listen(80, () => console.log('Listing port 80 (HTTP)'))
